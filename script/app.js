cd26dc9a6dd8381e996dd437018d35f43d33dfd8
jaybabaa <ewelunnamdi@gmail.com>
2021-10-31 02:15:40 +0100

    first commit

--- script/app.js
+++ script/app.js
@@ -9,10 +9,10 @@ function getPost() {
             // console.log(element)
             html += `
             <div class="row" id="post-card">
-                <div class="col-md-4 mb-3">
-                    <div class="card">
+                <div class="col-lg-4 mb-3">
+                    <div class="card h-100">
                         <div class="card-body">
-                             <div class="d-flex justify-content-center">
+                             <div class="d-flex justify-content-end">
                                 <h4 id="id-posts" class="text-danger">Post ${e.id}</h4>
                             </div>
                             <div>
@@ -20,14 +20,14 @@ function getPost() {
                                 <p class="posts-body text-center" id="body-posts">${e.body}r</p>
                             </div>
                             <div>
-                                <a href="#" class="text-decoration-none d-flex justify-content-center"><button type="submit" class=" btn-primary" onclick="window.location.href='reg.html'">Click me!</button></a>
+                            <a href="#" class="text-decoration-none d-flex justify-content-center"><button type="submit" class="btn-primary p-1" onclick="pages(${e.id})">Click me!</button></a>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
             `
-            console.log (e)
+            // console.log (e)
             postCard.innerHTML = html
         });
     })
@@ -35,3 +35,43 @@ function getPost() {
 
 getPost();
 
+
+function pages(id) {
+    localStorage.setItem("postid", id);
+    postId = localStorage.getItem("postid");
+    console.log(postId);
+
+    fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+    .then(response => response.json())
+    .then((e) => {
+        // console.log(data)
+        let postCard = document.getElementById('post-card')
+        // userPost = data
+        let html = "";
+        // data.forEach( e=> {
+            // console.log (element)
+            html = `
+                    <div class="col-md-4 mb-3">
+                        <div class="card h-100" onclick="pages()">
+                            <div class="card-body">
+                                <div class="d-flex justify-content-end">
+                                    <h4 id="id-posts" class="text-danger">Post ${e.id}</h4>
+                                </div>
+                                <div>
+                                    <h4 class="posts-title mb-4 text-center" id="title-posts"> ${e.title}</h4>
+                                    <p class="posts-body text-center" id="body-posts"> ${e.body}</p>
+                                </div>
+                                <div>
+                                    <a href="index.html" class="text-decoration-none d-flex justify-content-center"><button type="submit" class="btn-primary p-1" onclick="pages()">Go Back</button></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+            `
+            // console.log(e)
+            postCard.innerHTML = html
+        });
+//     })
+}
+
+passvalues(id);
