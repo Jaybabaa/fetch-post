282c847330833efb5a28c758c9e42c4e9b5be88f
jaybabaa <ewelunnamdi@gmail.com>
2021-11-16 02:22:55 +0100

    second commit

--- script/app.js
+++ script/app.js
@@ -8,21 +8,17 @@ function getPost() {
         data.forEach (e => {
             // console.log(element)
             html += `
-            <div class="row card-deck cool" id="post-card">
-                <div class="col col-lg-5 col-md-4 mb-3 ">
-                    <div class="card ">
-                        <div class="card-body">
-                             <div class="d-flex justify-content-end">
-                                <h4 id="id-posts" class="text-danger">Post ${e.id}</h4>
-                            </div>
-                            <div>
-                                <h4 class="posts-title mb-4 text-center" id="title-posts">${e.title}</h4>
-                                <p class="posts-body text-center" id="body-posts">${e.body}r</p>
-                            </div>
-                            <div>
-                            <a href="#" class="text-decoration-none d-flex justify-content-center"><button type="submit" class="btn-primary p-1" onclick="pages(${e.id})">Click me!</button></a>
-                            </div>
+            <div class="col-md-4 mb-3">
+                <div class="card">
+                    <div class="card-body" style="height: 300px;">
+                        <div class="d-flex justify-content-end">
+                        <h5 class="mx-2 text-danger">${e.id}</h5>
                         </div>
+                        <h5 class="post-title mb-4">${e.title}</h5>
+                        <p class="post-body">${e.body}</p>
+                    </div>
+                    <div class="text-center py-2">
+                        <button id="click" onclick="pagePost(event)" class="btn btn-primary" data-id=${e.id}> Read More</button>
                     </div>
                 </div>
             </div>
